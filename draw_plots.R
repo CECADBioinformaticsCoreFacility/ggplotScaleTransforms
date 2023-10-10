3c67111851ab619b92b5b264267945ca157239d8
Ulrike Göbel <ugoebel@uni-koeln.de>
2023-10-10 16:11:15 +0200

    simplified draw_plot.R; collect toy data in data/

--- draw_plots.R
+++ draw_plots.R
@@ -1,21 +1,18 @@
 library(ggplot2)
 
-data <- c(0.1, 1, 2, 2.5, 12, 9.45, 21.07, 91)
+source("R/helpers.R")
+source("R/interval_trans.R")
 
 
 data_intervals <-
   data.frame(from = c(0,  4,  8,  20),
              to   = c(4,  8 , 20,  Inf))
 
-
 scaling_factors <- c(1,     1 / 2,    1 / 3,   1 / 10)
 
 
-##w <- 10
-##Cairo::Cairo(file = "testcase.png", unit = "in", dpi = 300, width = w,
-##        height = w * 6/10, type = "png", bg = "white")
-p_y <- ggplot(tibble::tibble(x = c(10, 5, 2, 7, 10, 11, 20, 50),
-                             y = data),
+p_y <- ggplot(tibble::tibble(x = readr::read_tsv("data/xydata_1.tsv")$x,
+                             y = readr::read_tsv("data/xydata_1.tsv")$y),
               aes(x = x, y = y)) +
   ##theme_classic() +
   geom_point() +
@@ -23,10 +20,10 @@ p_y <- ggplot(tibble::tibble(x = c(10, 5, 2, 7, 10, 11, 20, 50),
   scale_y_continuous(trans = interval_trans(data_intervals,
                                             scaling_factors),
                      n.breaks = 50
-                     ##expand = c(0, 0) )
+                     ##expand = c(0, 0) 
+                     )
                      
-                     ##dev.off()
-                     
+
 ## ------------------------------------------------------------------------
 ## 2-axes scaling:
 ## ------------------------------------------------------------------------
@@ -41,15 +38,8 @@ y_data_intervals <-
 y_scaling_factors <- c(1 / 10,     1,    1 / 10)
 
 
-x_data <-
-  c(-2, 2, 21, 23,  24,   26.3, 26.9, 29, 50.3, 82,  87, 95, 105)
-y_data <- c(-4, 1, 2, 2.5, 9.45, 10.1, 12, 15, 41.6, 50.5, 60, 82, 91)
-
-##w <- 10
-##Cairo::Cairo(file = "double_scaling.png", unit = "in", dpi = 300, width = w,
-##        height = w * 6/10, type = "png", bg = "white")
-p_xy <- ggplot(tibble::tibble(x = x_data,
-                              y = y_data),
+p_xy <- ggplot(tibble::tibble(x = readr::read_tsv("data/xydata_2")$x,
+                              y = readr::read_tsv("data/xydata_2")$y),
                aes(x = x, y = y)) +
   geom_point() +
   
@@ -59,5 +49,4 @@ p_xy <- ggplot(tibble::tibble(x = x_data,
   scale_y_continuous(trans = interval_trans(y_data_intervals,
                                             y_scaling_factors),
                      n.breaks = 50)
-##dev.off()
 
